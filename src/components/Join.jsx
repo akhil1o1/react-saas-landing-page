73c71a732d4aba7439faf916f5a0cbcfd30c47ec
akhil panwar <akhilpanwar180@gmail.com>
2022-09-19 22:37:49 +0530

    completed subscribe section

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import {Box, Paper} from "@mui/material";
+import {Box, Paper, Typography, TextField, Button} from "@mui/material";
 
 function Join() {
-    return <Paper className="bottom-banner">
+    return <>
+        <Paper className="bottom-banner">
+        <Box display="flex" p="7%" flexDirection="row" alignItems="center">
+            <Box flex="0.5" p="5%" className="email-container">
+                <Typography variant="body2" mb="20px" color="#ffffff" fontWeight="500">
+                    JOIN WITH OUR COMMUNITY
+                </Typography>
+                <Typography variant="h4" mb="20px" fontSize="30px" fontWeight="800" color="#ffffff">
+                Set your social media growth on autopilot.
+                </Typography>
+                <Box display="flex" flexDirection="row">
+                <TextField className="textfield" id="outlined-basic" label="YOUR EMAIL ADDRESS" variant="outlined"/>
+                <Button className="subscribe-button" variant="contained">Subscribe</Button>
+                </Box>
+            </Box> 
+            <Box flex="0.5">
 
+            </Box>
+        </Box>
     </Paper>
+    </> 
 }
 
 export default Join;
